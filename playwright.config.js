52b37fbff71832831db59c2a16d2f798588ff2b8
Denis Matveev <denis.vic.matveev@gmail.com>
2026-03-28 02:54:47 +0100

    Removed visual regression

--- playwright.config.js
+++ playwright.config.js
@@ -24,12 +24,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  expect: {
-    toHaveScreenshot: {
-      pathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}{ext}',
-    },
-  },
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
